60ced55efd7031c432aaa20406730f7c4fc3b045
ScoutQuebecois <eloivl13@gmail.com>
2022-01-26 16:37:00 -0500

    Conformiser problem 4

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
+#include <iomanip>
 #include <math.h>
 #include <ctime>
 #include <cstdlib>
 
 using namespace std;
 
-const double valeurPI = 3.141593;
+const double VALEUR_PI = 3.141593;
 
-double randomIn(double min, double max) {
-    return ((double)rand() / (RAND_MAX)) * (max - min) - max;
+double randomDans(double minimum, double maximum) {
+    return ((double)rand() / (RAND_MAX)) * (maximum - minimum) - maximum;
 }
 
-void main() {
+int main(int argc, char* argv[]) {
+
     srand((unsigned)time(NULL));
+
+    double iteration = 1;
+    double dansLeCercle = 0;
+    double x = 0, y;
+
     cout.precision(7);
     cout << "Entrer le nombdre d'iterations souhaitees : ";
-    double iter = 1;
-    double dansLeCercle = 0;
-    double x, y;
-    cin >> iter;
-    for (int i = 1; i <= iter; i++) {
-        x = randomIn(-1, 1);
-        y = randomIn(-1, 1);
+    cin >> iteration;
+    
+    for (int i = 1; i <= iteration; i++) {
+        x = randomDans(-1, 1);
+        y = randomDans(-1, 1);
+
         if (pow(x, 2) + pow(y, 2) < 1) {
             dansLeCercle++;
         }
     }
-    double pi = (dansLeCercle / iter) * 4;
-    double ecart = (abs(pi - valeurPI) / valeurPI) * 100;
-    cout << "Votre valeur approxime de pi est de " << pi << ". L'ecart relatif entre cette valeur et la valeur precise de pi est de " << ecart << "%.";
+
+    double pi = (dansLeCercle / iteration) * 4;
+    double ecart = (abs(pi - VALEUR_PI) / VALEUR_PI) * 100;
+    cout.fill('0');
+    cout << "Votre valeur approxime de pi est de " <<  std::setw(6)<< left << pi << ". \n"
+         <<"L'ecart relatif entre cette valeur et la valeur precise de pi est de " << std::setw(6) << left << ecart << "%.";
 }
